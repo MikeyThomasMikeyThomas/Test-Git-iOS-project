b9ae8c62bc6357f9159b9888d247dd5d4b39b8e4
Michael Thomas <Mikey@Michaels-MacBook-Pro.local>
2014-04-15 17:08:12 -0400

    added a test class

---
+++ MTtest.h
@@ -0,0 +1,13 @@
+//
+//  MTtest.h
+//  Git Test
+//
+//  Created by Michael Thomas on 4/15/14.
+//  Copyright (c) 2014 Michael Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTtest : NSObject
+
+@end
---
+++ MTtest.m
@@ -0,0 +1,13 @@
+//
+//  MTtest.m
+//  Git Test
+//
+//  Created by Michael Thomas on 4/15/14.
+//  Copyright (c) 2014 Michael Thomas. All rights reserved.
+//
+
+#import "MTtest.h"
+
+@implementation MTtest
+
+@end
